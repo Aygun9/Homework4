889f539b62adc855d6f1f55b845528b73f08ba2c
Aygun <aygun.gasimova@inbox.ru>
2022-12-04 23:58:41 +0400

    Task25 completed

---
+++ Task25/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int FindAPowB(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+
+Console.WriteLine("insert number A : ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("insert number B : ");
+int numberB = int.Parse(Console.ReadLine());
+int result = FindAPowB( numberA, numberB);
+Console.WriteLine(result);
